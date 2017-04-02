d2427c61d56f71f947496556b2455f94d2dffca9
Jacek Byczkiewicz <jck53416@gmail.com>
2017-04-02 22:19:11 +0200

    Implemented: showing the informations about currently running instace: /info

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var lab1_1 = require("./lab/lab1_1").lab
 var example_1 = require("./example_1").lab;
+var lab1_2 = require("./lab/lab1_2").lab;
 
-var PORT = 8080;
+var PORT = 1337;
 
 
 var urlMap = [
 	{path: "/", action:__dirname + "/static/index.html"},	 
 	{path: "/digest", action: lab1_1},	
-	{path: "/example_1", action: example_1}, 
+    { path: "/example_1", action: example_1 },
+    { path: "/info", action: lab1_2 },
 	];
 
 var service = require("./lib/service").http(urlMap);
--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -4,8 +4,30 @@ var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json');
 
 var task =  function(request, callback){
-	
-	
+
+    var ec2 = new AWS.EC2({
+        region: 'us-west-2'
+    });
+
+    var params = {
+        Filters: [
+            {
+                Name: 'instance-state-name',
+                Values: ['running']
+            }
+        ]
+    };
+    ec2.describeInstances(params, function (err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            callback(err, err.stack); // an error occurred
+        }
+        else {
+            console.log(data);
+            callback(null, data);
+        }           // successful response
+    });
+   	
 }
 
 exports.lab = task
